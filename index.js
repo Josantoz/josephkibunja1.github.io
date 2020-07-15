558137a63f3d6c614ce35bdfbd61b23826513ef2
Joseph Maingi <jkmaingi1@gmail.com>
2020-07-15 14:40:11 +0300

    Portfolio 1

---
+++ index.js
@@ -0,0 +1,28 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction();};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+/* Add the sticky class to the navbar when you reach its scroll position. 
+Remove "sticky" when you leave the scroll position */
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+$(function() {
+  $('.js-nav a, .js-connect').click(function(e) {
+    e.preventDefault();
+    $('body, html').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 750);
+  });
+});
